64cd94f25560514be337b537c0d64265dfe14eb5
Dev <91795740+cswong1991@users.noreply.github.com>
2022-04-10 23:27:54 +0800

    init

---
+++ OptionsController.js
@@ -0,0 +1,83 @@
+class OptionsController {
+    constructor() {
+        document.getElementById('save').addEventListener('click', () => this.requestSave());
+        document.getElementById('cancel').addEventListener('click', () => this.cancel());
+        document.getElementById('load_default').addEventListener('click', () => this.loadDefault());
+        this.errors = {};
+    }
+
+    loadSettings() {
+        chrome.storage.local.get(['target_windows', 'target_tabs', 'bypass_cache', 'exclude_audible', 'exclude_urls'], function (data) {
+            document.getElementById(data['target_windows'] ?? 'all_windows').checked = true;
+            document.getElementById(data['target_tabs'] ?? 'all_tabs').checked = true;
+            document.getElementById('bypass_cache').checked = data['bypass_cache'] ?? false;
+            document.getElementById('exclude_audible').checked = data['exclude_audible'] ?? false;
+            document.getElementById('exclude_urls').value = data['exclude_urls'] ? data['exclude_urls'].join('\n') : '';
+        });
+    }
+
+    requestSave() {
+        let target_windows = [].slice.call(document.getElementsByName('target_windows'), 0).filter(e1 => e1.checked)[0].value;
+        let target_tabs = [].slice.call(document.getElementsByName('target_tabs'), 0).filter(e1 => e1.checked)[0].value;
+        let bypass_cache = document.getElementById('bypass_cache').checked;
+        let exclude_audible = document.getElementById('exclude_audible').checked;
+        let exclude_urls = document.getElementById('exclude_urls').value;
+
+        if (this.validateSave(target_windows, target_tabs, bypass_cache, exclude_audible, exclude_urls)) {
+            this.save(target_windows, target_tabs, bypass_cache, exclude_audible, exclude_urls);
+        }
+        document.getElementById('msg').innerHTML = JSON.stringify(this.errors);
+    }
+
+    validateSave(target_windows, target_tabs, bypass_cache, exclude_audible, exclude_urls) {
+        this.errors = {};
+        if (!['all_windows', 'this_window'].includes(target_windows)) {
+            this.errors['target_windows'] = 'Invalid target_windows';
+        }
+        if (!['all_tabs', 'pinned_tabs', 'unpinned_tabs'].includes(target_tabs)) {
+            this.errors['target_tabs'] = 'Invalid target_tabs';
+        }
+        if (typeof bypass_cache !== 'boolean') {
+            this.errors['bypass_cache'] = 'Invalid bypass_cache';
+        }
+        if (typeof exclude_audible !== 'boolean') {
+            this.errors['exclude_audible'] = 'Invalid exclude_audible';
+        }
+        // Must be an array with min length of 1
+        let urls_inArr = exclude_urls.split('\n');
+        if (exclude_urls.length > 0 && urls_inArr.some(e1 => e1.trim() === '')) {
+            this.errors['exclude_urls'] = 'Contain empty lines'
+        } else if (new Set(urls_inArr).size !== urls_inArr.length) {
+            this.errors['exclude_urls'] = 'Contain identical urls or domains'
+        }
+        return Object.keys(this.errors).length === 0;
+    }
+
+    save(target_windows, target_tabs, bypass_cache, exclude_audible, exclude_urls) {
+        chrome.storage.local.set({
+            target_windows: target_windows,
+            target_tabs: target_tabs,
+            bypass_cache: bypass_cache,
+            exclude_audible: exclude_audible,
+            exclude_urls: (exclude_urls.length === 0) ? [] : exclude_urls.split('\n')
+        }, function () {
+            window.close();
+        });
+    }
+
+    cancel() {
+        window.close();
+    }
+
+    loadDefault() {
+        document.getElementById('all_windows').checked = true;
+        document.getElementById('all_tabs').checked = true;
+        document.getElementById('bypass_cache').checked = false;
+        document.getElementById('exclude_audible').checked = false;
+        document.getElementById('exclude_urls').value = '';
+    }
+}
+
+let main = new OptionsController();
+main.loadSettings();
+
---
+++ background.js
@@ -0,0 +1,121 @@
+chrome.runtime.onInstalled.addListener(function () {
+    loadSettings(createContextmenus);
+});
+
+chrome.action.onClicked.addListener(function (tab) {
+    loadSettings(requestReload);
+});
+
+chrome.storage.onChanged.addListener(function (changes, areaName) {
+    if (changes['bypass_cache'] || changes['exclude_audible']) {
+        loadSettings(createContextmenus);
+    }
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    switch (info.menuItemId) {
+        case 'all_windows': loadSettings((settings) => { settings['target_windows'] = 'all_windows'; requestReload(settings); }); break;
+        case 'this_window': loadSettings((settings) => { settings['target_windows'] = 'this_window'; requestReload(settings); }); break;
+        case 'pinned_tabs': loadSettings((settings) => { settings['target_tabs'] = 'pinned_tabs'; requestReload(settings); }); break;
+        case 'unpinned_tabs': loadSettings((settings) => { settings['target_tabs'] = 'unpinned_tabs'; requestReload(settings); }); break;
+        case 'bypass_cache': chrome.storage.local.set({ bypass_cache: info.checked }); break;
+        case 'exclude_audible': chrome.storage.local.set({ exclude_audible: info.checked }); break;
+    }
+});
+
+function loadSettings(callback) {
+    chrome.storage.local.get(['target_windows', 'target_tabs', 'bypass_cache', 'exclude_audible', 'exclude_urls'], function (data) {
+        let settings = JSON.parse(JSON.stringify(data));
+        settings['target_windows'] = settings['target_windows'] ?? 'all_windows';
+        settings['target_tabs'] = settings['target_tabs'] ?? 'all_tabs';
+        settings['bypass_cache'] = settings['bypass_cache'] ?? false;
+        settings['exclude_audible'] = settings['exclude_audible'] ?? false;
+        settings['exclude_urls'] = settings['exclude_urls'] ?? [];
+        console.log(settings);
+        if (callback) {
+            callback(settings);
+        }
+    });
+}
+
+function createContextmenus(settings) {
+    chrome.contextMenus.removeAll(() => {
+        let all_windows = {
+            id: 'all_windows',
+            title: 'Reload All Windows',
+            type: 'normal',
+            checked: settings['target_windows'] === 'all_windows',
+            contexts: ['action']
+        }
+        let this_window = {
+            id: 'this_window',
+            title: 'Reload This Window',
+            type: 'normal',
+            checked: settings['target_windows'] === 'this_window',
+            contexts: ['action']
+        }
+        let pinned_tabs = {
+            id: 'pinned_tabs',
+            title: 'Reload Pinned Tabs',
+            type: 'normal',
+            checked: settings['target_tabs'] === 'pinned_tabs',
+            contexts: ['action']
+        }
+        let unpinned_tabs = {
+            id: 'unpinned_tabs',
+            title: 'Reload Unpinned Tabs',
+            type: 'normal',
+            checked: settings['target_tabs'] === 'unpinned_tabs',
+            contexts: ['action']
+        }
+        let bypass_cache = {
+            id: 'bypass_cache',
+            title: 'Bypass Cache',
+            type: 'checkbox',
+            checked: settings['bypass_cache'],
+            contexts: ['action']
+        }
+        let exclude_audible = {
+            id: 'exclude_audible',
+            title: 'Exclude Audible',
+            type: 'checkbox',
+            checked: settings['exclude_audible'],
+            contexts: ['action']
+        }
+        chrome.contextMenus.create(all_windows);
+        chrome.contextMenus.create(this_window);
+        chrome.contextMenus.create(pinned_tabs);
+        chrome.contextMenus.create(unpinned_tabs);
+        chrome.contextMenus.create(bypass_cache);
+        chrome.contextMenus.create(exclude_audible);
+    });
+}
+
+async function requestReload(settings) {
+    let queryInfo = {};
+    let exclude_tabs = [];
+    let exclude_tabIDs = [];
+
+    if (settings['target_windows'] === 'this_window') {
+        queryInfo['currentWindow'] = true;
+    }
+    if (['pinned_tabs', 'unpinned_tabs'].includes(settings['target_tabs'])) {
+        queryInfo['pinned'] = (settings['target_tabs'] === 'pinned_tabs');
+    }
+    if (settings['exclude_audible']) {
+        queryInfo['audible'] = false;
+    }
+    if (settings['exclude_urls'].length > 0) {
+        exclude_tabs = await chrome.tabs.query({ url: settings['exclude_urls'] });
+        exclude_tabIDs = exclude_tabs.map(e1 => e1['id']);
+    }
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        tabs.forEach(thisTab => {
+            if (exclude_tabIDs.includes(thisTab['id'])) {
+                return;
+            }
+            chrome.tabs.reload(thisTab['id'], { bypassCache: settings['bypass_cache'] });
+        });
+    })
+}
